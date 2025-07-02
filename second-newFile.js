cdf1cf9294f1c904ea3563b0ad73a9e1542f72f2
Joe <a.m.sadra4@gmail.com>
2025-07-03 02:21:29 +0330

    fixing error from second-newFile.js with git reset option

--- second-newFile.js
+++ second-newFile.js
@@ -1,3 +1,20 @@
 
 
 console.log("this is second file!")
+
+
+async function goodFunction() {
+    // console.log("this function is causing some errors!");
+    console.log("but we commit it befor and now we don't know this function is the reson of error!");
+    console.log(`so we just use "git reset commit-hash" to see all changes appeared after that commit hash and fix theme all!`);
+    console.log(`you can grab commit-hash from "git log"`);
+}
+
+
+
+// async function badFunction() {
+//     console.log("this function is causing some errors!");
+//     console.log("but we commit it befor and now we don't know this function is the reson of error!");
+//     console.log(`so we just use "git reset commit-hash" to see all changes appeared after that commit hash and fix theme all!`);
+//     console.log(`you can grab commit-hash from "git log"`);
+// }
